daa55c94c4ab7da8464a004b689c59e551181b59
AnhHuynh1999 <74520649+AnhHuynh1999@users.noreply.github.com>
2023-05-20 17:15:13 +0700

    Add_Delete_Todo

--- src/views/Todos/ListTodo.js
+++ src/views/Todos/ListTodo.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { toast } from "react-toastify";
 import AddTodo from "./AddTodo";
 import "./ListTodo.scss";
 class ListTodo extends React.Component {
@@ -8,6 +9,7 @@ class ListTodo extends React.Component {
       { id: "todo2", title: "Making video" },
       { id: "todo3", title: "Fixing bugs" },
     ],
+    editTodo: {},
   };
 
   addNewTodo = (todo) => {
@@ -16,21 +18,83 @@ class ListTodo extends React.Component {
     });
   };
 
+  handleDeleteTodo = (todo) => {
+    this.setState({
+      listTodos: this.state.listTodos.filter((item) => item.id !== todo.id),
+    });
+    toast.success("Delete successfully");
+  };
+
+  handleEditTodo = (todo) => {
+    let { editTodo, listTodos } = this.state;
+    let isEmtyObj = Object.keys(editTodo).length === 0;
+    //save
+    if (isEmtyObj === false && editTodo && editTodo.id === todo.id) {
+      let listTodosCopy = [...listTodos];
+      let objIndex = listTodos.findIndex((item) => item.id === todo.id);
+      listTodosCopy[objIndex].title = editTodo.title;
+      this.setState({
+        listTodos: listTodosCopy,
+        editTodo: {},
+      });
+      toast.success("Edit successfully");
+      return;
+    }
+    //edit
+    this.setState({ editTodo: todo });
+  };
+
+  handleChangeTitle = (e) => {
+    this.setState({
+      editTodo: { ...this.state.editTodo, title: e.target.value },
+    });
+  };
+
+  handleSaveTodo = () => {
+    this.setState({
+      listTodos: [...this.state.listTodos],
+      editTodo: {},
+    });
+  };
+
   render() {
-    const { listTodos } = this.state;
+    const { listTodos, editTodo } = this.state;
+    let isEmtyObj = Object.keys(editTodo).length === 0;
     return (
       <div className="list-todo-container">
-        <AddTodo addNewTodo={this.addNewTodo}/>
+        <AddTodo addNewTodo={this.addNewTodo} />
         <div className="list-todo-content">
           {listTodos &&
             listTodos.length > 0 &&
             listTodos.map((todo, index) => (
               <div className="todo-child" key={index}>
-                <span>
-                  {index + 1} - {todo.title}
-                </span>
-                <button className="edit">Edit</button>
-                <button className="delete">Delete</button>
+                {isEmtyObj === false && editTodo.id === todo.id ? (
+                  <span>
+                    {index + 1} -{" "}
+                    <input
+                      value={editTodo.title}
+                      onChange={(e) => this.handleChangeTitle(e)}
+                    />
+                  </span>
+                ) : (
+                  <span>
+                    {index + 1} - {todo.title}
+                  </span>
+                )}
+                <button
+                  className="edit"
+                  onClick={() => this.handleEditTodo(todo)}
+                >
+                  {isEmtyObj === false && editTodo.id === todo.id
+                    ? "Save"
+                    : "Edit"}
+                </button>
+                <button
+                  className="delete"
+                  onClick={() => this.handleDeleteTodo(todo)}
+                >
+                  Delete
+                </button>
               </div>
             ))}
         </div>
